69e510b1495a9268c0e123714bdfd48fb8150108
Maria Eduarda Zuim Lamarca <maria.lamarca@aluno.senai.br>
2024-05-07 10:13:06 -0300

    positions

---
+++ js/app.js
@@ -0,0 +1,22 @@
+function mensagem()
+{
+    alert("BOM DIA!!!");
+}
+
+function alertaCorDeFundo()
+{
+    document.body.style.backgroundColor = "#FF0000";
+    let h1 = document.querySelector("h1");
+    h1.style.color = "#FFF";
+
+    let texto = document.querySelector("p");
+    texto.style.fontSize = "60px";
+    texto.style.color = "#FFFF00";
+
+    let info = document.querySelector(".info");
+}    
+
+function ResetarCor()
+{
+    document.body.style.backgroundColor = "#FFF";
+}
